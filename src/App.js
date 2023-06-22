aec58c6459f5ca05379265ba82d6299a2305be8f
Colin Huber <cbhuber@gmail.com>
2023-06-21 22:57:51 -0600

    feat: lift up state to render data from the form on the packing list.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 1, packed: true },
+// ];
 
 export default function App() {
+  // Declared here since state is set in child Form(), rendered in PackingList().
+  // App() is the parent of these siblings.
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    // Cannot do items.push() as arrays are immutable in react
+    // Rather create a new array with the current items plus append the new one
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />;
-      <Form />;
-      <PackingList />;
+      <Form onAddItems={handleAddItems} />;
+      <PackingList items={items} />;
       <Stats />;
     </div>
   );
@@ -21,22 +31,24 @@ function Logo() {
   return <h1>🌴 Far Away 🎒</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    // Guard clause; prevent submit with no description
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    // Reset to initial state
+    setDescription("");
+    setQuantity(1);
   }
-
-  // Guard clause; prevent submit with no description
-  if (!description) return;
-
-  const newItem = { description, quantity, packed: false, id: Date.now() };
-
-  // Reset to initial state
-  setDescription("");
-  setQuantity(1);
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
@@ -62,11 +74,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
